cb64fcdfdc74e488b63b9a58ce5e68222dbf12b0
Hirecheanis <hireche2015anis14@gmail.com>
2024-08-15 13:41:06 +0100

    day 2

--- C/AOC2015/day2/main.c
+++ C/AOC2015/day2/main.c
@@ -1,25 +1,54 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+// part 1
+long wrapper(FILE *input_file)
+{
+    int c;
+    if (input_file == 0)
+    {
+        perror("there was an error reading the file\n");
+        exit(-1);
+    }
+    else
+    {
+        while ((c = fgetc(input_file)) != EOF)
+        {
+
+            if (isdigit(c) ){
+            int i = c - '0';
+            printf("%d",i);
+            }
+            else if (c == 'x'){
+                
+                
+                    printf("%c", c);}
+                
+            else if(c == '\n'){
+
+                printf("\n");
+            
+            }
+        }
+    }
+    return 5;
+}
 
 int main(int argc, char **argv)
 {
-   // printf("hello world!\n");
-
-
     char *input = argv[1];
-    int i = 1;
-    char  result[] = "";
-    
-    while (argv[i])
-    {
-        
-        printf("%c\n", *argv[i]);
-        char *temp =""+ *argv[i];
-        printf("%s",temp);
-        strcat(result, temp);
-        i++;
-    }
-    
-    printf("you said %s lmao! \n", result);
+    FILE *input_file;
+    input_file = fopen(input, "r");
+
+    long result = wrapper(input_file);
+
+    printf("result = %d \n", result);
+
     return 0;
 }
